d95f77a38ef82c93e785175e35f3d17893d5825d
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-13 08:26:25 -0300

    Feat: add initial Trip Details Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateTrip from "./pages/create-trip";
+import TripDetailsPage from "./pages/trip-details";
 
 const router = createBrowserRouter([
    {
       path: "/",
       element: <CreateTrip />,
+   },
+
+   {
+      path: "/trips/:tripId",
+      element: <TripDetailsPage />,
    },
 ]);
 
---
+++ src/pages/trip-details/index.tsx
@@ -0,0 +1,5 @@
+const TripDetailsPage = () => {
+   return <h1>Trip details</h1>;
+};
+
+export default TripDetailsPage;
