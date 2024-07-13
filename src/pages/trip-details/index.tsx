7373afbbe6dd12153f4c9e78004233083c5a4a72
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-13 10:34:55 -0300

    Feat: add initial activity session

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 
 const TripDetailsPage = () => {
    return (
@@ -22,6 +22,71 @@ const TripDetailsPage = () => {
                </button>
             </div>
          </header>
+
+         <main className="flex gap-16 px-4">
+            <section className="flex-1 space-y-6">
+               <div className="flex items-center justify-between">
+                  <h2 className="text-3xl font-semibold">Atividades</h2>
+
+                  <button className="flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400">
+                     <Plus className="size-5" />
+                     Cadastrar atividade
+                  </button>
+               </div>
+
+               <div className="space-y-8">
+                  <div className="space-y-2.5">
+                     <div className="flex gap-2 items-baseline">
+                        <span className="text-xl font-semibold text-zinc-300">
+                           Dia 17
+                        </span>
+                        <span className="text-xs text-zinc-500">Sábado</span>
+                     </div>
+
+                     <p className="text-zinc-500 text-sm">
+                        Nenhuma atividade cadastrada nessa data.
+                     </p>
+                  </div>
+
+                  <div className="space-y-2.5">
+                     <div className="flex gap-2 items-baseline">
+                        <span className="text-xl font-semibold text-zinc-300">
+                           Dia 18
+                        </span>
+                        <span className="text-xs text-zinc-500">Domingo</span>
+                     </div>
+
+                     <div className="space-y-2.5">
+                        <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                           <CircleCheck className="size-5 text-lime-300" />
+                           <span className="text-zinc-100">
+                              Academia em grupo
+                           </span>
+
+                           <span className="text-zinc-400 tetx-sm ml-auto">
+                              08:00h
+                           </span>
+                        </div>
+                     </div>
+
+                     <div className="space-y-2.5">
+                        <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                           <CircleCheck className="size-5 text-lime-300" />
+                           <span className="text-zinc-100">
+                              Academia em grupo
+                           </span>
+
+                           <span className="text-zinc-400 tetx-sm ml-auto">
+                              08:00h
+                           </span>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+
+            <section className="w-80"></section>
+         </main>
       </div>
    );
 };
