bc249c18ed584cc9a78c39f1b21de42d0f36dd9b
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-13 10:14:05 -0300

    Feat: add Trip Details Page header

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,5 +1,29 @@
+import { Calendar, MapPin, Settings2 } from "lucide-react";
+
 const TripDetailsPage = () => {
-   return <h1>Trip details</h1>;
+   return (
+      <div className="max-w-6xl py-10 mx-auto space-y-8">
+         <header className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+            <div className="flex items-center gap-2">
+               <MapPin className="size-5 text-zinc-400" />
+               <span className="text-zinc-100">Florianópolis, Brasil</span>
+            </div>
+
+            <div className="flex items-center gap-5">
+               <div className="flex items-center gap-2">
+                  <Calendar className="size-5 text-zinc-400" />
+                  <span className="text-zinc-100">17 a 23 de adosto</span>
+               </div>
+
+               <div className="w-px h-6 bg-zinc-800"></div>
+
+               <button className="flex items-center gap-2 bg-zinc-800 text-zinc-300 rounded-lg px-5 py-2 font-medium hover:bg-zinc-700">
+                  Alterar local/data <Settings2 className="size-5" />
+               </button>
+            </div>
+         </header>
+      </div>
+   );
 };
 
 export default TripDetailsPage;
