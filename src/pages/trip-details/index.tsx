b6aa95bd537944530ae9bffc5465d8e7e4affe25
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-13 18:40:38 -0300

    Feat: add Guests component

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,15 +1,8 @@
-import {
-   Calendar,
-   CircleCheck,
-   CircleDashed,
-   MapPin,
-   Plus,
-   Settings2,
-   UserCog,
-} from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 import { useState } from "react";
 import CreateActivityModal from "./components/create-activity-modal";
 import ImportantLinks from "./components/important-links";
+import Guests from "./components/guests";
 
 const TripDetailsPage = () => {
    const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -114,40 +107,7 @@ const TripDetailsPage = () => {
 
                <div className="w-full h-px bg-zinc-800"></div>
 
-               <div className="space-y-6">
-                  <h2 className="font-semibold text-xl">Convidados</h2>
-
-                  <div className="space-y-5">
-                     <div className="flex items-center justify-between gap-4">
-                        <div className="space-y-1.5 text">
-                           <span className="block font-medium text-zinc-100">
-                              Jessica White
-                           </span>
-                           <span className="block text-sm text-zinc-400 truncate">
-                              jessica.white44@yahoo.com
-                           </span>
-                        </div>
-                        <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-                     </div>
-
-                     <div className="flex items-center justify-between gap-4">
-                        <div className="space-y-1.5 text">
-                           <span className="block font-medium text-zinc-100">
-                              Dr. Rita Pacocha
-                           </span>
-                           <span className="block text-sm text-zinc-400 truncate">
-                              lacy.stiedemann@gmail.com
-                           </span>
-                        </div>
-                        <CircleDashed className="size-5 text-zinc-400 shrink-0" />
-                     </div>
-                  </div>
-
-                  <button className="flex items-center gap-2 w-full bg-zinc-800 justify-center text-zinc-300 rounded-lg px-5 h-11 font-medium hover:bg-zinc-700">
-                     <UserCog className="size-5" />
-                     Gerenciar convidados
-                  </button>
-               </div>
+               <Guests />
             </section>
          </main>
 
