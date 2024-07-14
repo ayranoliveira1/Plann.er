2f5e50171590f212072c6fbc1bd810397ccb115a
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-14 14:06:42 -0300

    Feat: add location and date of travel from the api

--- src/pages/trip-details/components/destination-and-date-header.tsx
+++ src/pages/trip-details/components/destination-and-date-header.tsx
@@ -1,18 +1,46 @@
 import { Calendar, MapPin, Settings2 } from "lucide-react";
 import Button from "../../../components/button";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { api } from "../../../lib/api";
+import { format } from "date-fns";
+
+interface Trip {
+   id: string;
+   destination: string;
+   starts_at: string;
+   ends_at: string;
+   is_confirmed: boolean;
+}
 
 const DestinationAndDateHeader = () => {
+   const [trip, setTrip] = useState<Trip | undefined>();
+
+   const { tripId } = useParams();
+
+   useEffect(() => {
+      api.get(`trips/${tripId}`).then((response) => {
+         setTrip(response.data.trip);
+      });
+   }, [tripId]);
+
+   const displayedDate = trip
+      ? format(trip.starts_at, "dd' de 'LLL")
+           .concat(" até ")
+           .concat(format(trip.ends_at, "dd' de 'LLL"))
+      : null;
+
    return (
       <header className="px-4 h-16 rounded-xl bg-zinc-90c0 shadow-shape flex items-center justify-between">
          <div className="flex items-center gap-2">
             <MapPin className="size-5 text-zinc-400" />
-            <span className="text-zinc-100">Florianópolis, Brasil</span>
+            <span className="text-zinc-100">{trip?.destination}</span>
          </div>
 
          <div className="flex items-center gap-5">
             <div className="flex items-center gap-2">
                <Calendar className="size-5 text-zinc-400" />
-               <span className="text-zinc-100">17 a 23 de adosto</span>
+               <span className="text-zinc-100">{displayedDate}</span>
             </div>
 
             <div className="w-px h-6 bg-zinc-800"></div>
