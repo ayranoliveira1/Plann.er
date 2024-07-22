448d828c76506aa87e0d3ffccf81b5e792461419
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-07-22 15:15:54 -0300

    Refactor: removing app component and adding information to the main page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import ToastProvider from "./components/toast";
-import { App } from "./app.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./pages/create-trip";
+import TripDetailsPage from "./pages/trip-details";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <CreateTrip />,
+   },
+
+   {
+      path: "/trips/:tripId",
+      element: <TripDetailsPage />,
+   },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <ToastProvider>
-         <App />
+         <RouterProvider router={router} />;
       </ToastProvider>
    </React.StrictMode>
 );
