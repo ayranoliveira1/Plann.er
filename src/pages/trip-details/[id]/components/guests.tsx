669db861b7510c60f577802c06af8a3083a61639
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-08-19 07:29:36 -0300

    remove: remove test git

--- src/pages/trip-details/[id]/components/guests.tsx
+++ src/pages/trip-details/[id]/components/guests.tsx
@@ -89,8 +89,6 @@ const Guests = () => {
                            guestId={participant.id}
                         />
                      )}
-
-                     <div>test 2</div>
                   </div>
                </div>
             ))}
